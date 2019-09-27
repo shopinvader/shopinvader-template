cc23d4d9aab6455adaec872477bcb4c23f48826f
thibault <thibault.rey@akretion.com>
2019-09-27 16:12:57 +0200

    improve autocomplete menu

--- app/assets/javascripts/sections/header.js
+++ app/assets/javascripts/sections/header.js
@@ -10,17 +10,21 @@ import {
   Layout,
   NoHitsErrorDisplay,
 } from 'searchkit';
+import {
+  Button
+} from 'react-bootstrap';
 import ProductHit from '../components/products.js';
 const Section = {
 
   load: (section) => {
-    console.log(section);
     Section.set_autocomplete(section);
   },
   set_autocomplete(section) {
     if(document.querySelector('.section-searchkit-faceting') == null) {
       //Only if it's not a faceting page
-      
+      function translate(key){
+        return searchkit_translation[key];
+      }
       var get = require("lodash/get");
       var container = section.querySelector('.header-block-search .container');
       const locale = container.dataset.locale.replace('_', '-');
@@ -28,9 +32,43 @@ const Section = {
         //PATCH FOR COMPATIBILITY WITH ELASTICSEARCH v7
         return get(this.results, ["hits", "total", "value"], 0);
       };
+      class HitsAutocomplete extends Hits {
+        constructor(props) {
+          super(props);
+        }
 
-      
-     
+        render() {
+          var query = this.getQuery();
+          if(query.index.queryString != '') {
+            var render = super.render();
+            var results = this.getResults();
+            var button = null;
+            console.log(results);
+            
+            if(results != null && results.hits.total.value > 0) {
+              button = (
+                <div className="text-center p-2 pb-3" key="autocomplete-allresult">
+                  <Button
+                    variant="outline-primary"
+                    type="button"
+                    size="sm"
+                    onClick={(item) => {
+                      document.location= './search?q='+encodeURI(query.index.queryString);
+                    }}
+                  >
+                    {translate('facets.view_all')}
+                  </Button>
+                </div>
+              );
+            }
+            return [
+              render,
+              button
+            ];
+          }
+          return null;
+        }
+      }
       class HitListItem extends ProductHit {
         constructor(props) {
           super(props, locale);
@@ -48,12 +86,28 @@ const Section = {
 
           this.state = {
             results: 0,
+            search_value: '',
             searchkit: searchkit
           };
-          searchkit.addResultsListener((results) => {
+          var parent = this;
+          searchkit.setQueryProcessor((query_params) => {
             
-            console.log('addResultsListener', );
+            if(query_params.hasOwnProperty('query')) {
+              parent.state.search_value = query_params.query.query;
+            }
+            else {
+              parent.state.search_value = '';
+            }
+
+            console.log('setQueryProcessor', query_params);
+            return query_params;
           });
+        }
+        test() {
+          if(this.state.search_value != '') {
+            return 'visible';
+          }
+          return '';
         }
         menu_visible() {
           if(this.state.menu_visible) {
@@ -61,27 +115,35 @@ const Section = {
           }
         }
         render() {
-          var mods = "sk-hits-grid";
+          var autocomplete_css = [
+            'autocomplete'
+          ];
+          if(this.state.search_value != '') {
+            autocomplete_css.push('visible');
+          }
+          
           return (
             <SearchkitProvider searchkit={this.state.searchkit}>
               <Layout>
                 <SearchBox
-                  autofocus={false}
+                  autofocus={true}
                   searchOnChange={true}
                   queryOptions={{analyzer:"standard"}}
                   queryFields={["name", "model.name^3", "short_description", "description"]}
-                />
-                <div className="autocomplete">
+                 />
+                <div className={'autocomplete '+this.test()}>
                   <NoHits 
                     errorComponent={NoHitsErrorDisplay}
                     suggestionsField="name"
                   />
-                  <Hits 
+                  <HitsAutocomplete
                     hitsPerPage={5} 
                     highlightFields={["title"]} 
-                    mod={mods} 
-                    itemComponent={HitListItem}
+                    mod={"sk-hits-grid "+Math.random()} 
+                    itemComponent={HitListItem} 
                   />
+                  <a href={'./search?'}>
+                  </a>
                 </div>
               </Layout>
             </SearchkitProvider>
@@ -92,24 +154,7 @@ const Section = {
         <App />, container
       );
     }
-  },
-  // unload: (section) => {
-  // },
-
-  // select: (section) => {
-  // },
-
-  // deselect: (section) => {
-  // },
-
-  // reorder: (section) => {
-  // },
-
-  // blockSelect: (section, block) => {
-  // },
-
-  // blockDeselect: (section, block) => {
-  // }
+  }
 
 }
 
