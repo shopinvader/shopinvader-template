e11a7b8f15a873c5b78b0dc8f33a46596862a3d6
thibault <thibault.rey@akretion.com>
2019-09-27 19:20:26 +0200

    clean files and debug value

--- app/assets/javascripts/sections/index.js
+++ app/assets/javascripts/sections/index.js
@@ -6,3 +6,5 @@
 
 export { default as SearchkitFaceting } from './searchkit_faceting';
 export { default as Header } from './header';
+export { default as Carousel } from './carousel';
+export { default as DynamicCarousel } from './dynamic_carousel';
