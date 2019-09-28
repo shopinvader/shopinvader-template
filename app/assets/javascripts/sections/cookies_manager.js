273cfcdd7bc2d06de0b0fc54f78c8388c77b64a7
thibault <thibault.rey@akretion.com>
2019-09-29 01:17:00 +0200

    improve coolie consent manager

---
+++ app/assets/javascripts/sections/cookies_manager.js
@@ -0,0 +1,28 @@
+
+const Section = {
+
+  load: (section) => {
+
+  },
+
+  // unload: (section) => {
+  // },
+
+  // select: (section) => {
+  // },
+
+  // deselect: (section) => {
+  // },
+
+  // reorder: (section) => {
+  // },
+
+  // blockSelect: (section, block) => {
+  // },
+
+  // blockDeselect: (section, block) => {
+  // }
+
+}
+
+export default Section;
